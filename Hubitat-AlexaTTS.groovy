d70083977e00f1bf08decb2abc0b8904aef956b3
cwwilson08 <33009556+cwwilson08@users.noreply.github.com>
2018-10-07 17:46:34 -0700

    Update Hubitat-AlexaTTS.groovy

--- Hubitat-AlexaTTS.groovy
+++ Hubitat-AlexaTTS.groovy
@@ -14,6 +14,7 @@ import groovy.json.JsonSlurper
 *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *  for the specific language governing permissions and limitations under the License.
 *
+*  10/07/2018 - Added support to display various device info - Chris Wilson
 *  9/2/2018 - Initial Release - Chris Wilson
 */
 
